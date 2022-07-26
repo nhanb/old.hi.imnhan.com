06883f5f6c0c66b9bcdebf3f681e95c102c0f780
Nhân <hi@imnhan.com>
2022-07-26 20:58:17 +0700

    play nice with people who prefer http

--- publishconf.py
+++ publishconf.py
@@ -2,16 +2,16 @@
 # -*- coding: utf-8 -*- #
 import os
 import sys
+
 sys.path.append(os.curdir)
 
 from pelicanconf import *  # noqa
-
 
 # This file is only used if you use `make publish` or
 # explicitly specify it as your config file.
 
 SITEURL = "https://hi.imnhan.com"
-RELATIVE_URLS = False
+RELATIVE_URLS = True
 
 DELETE_OUTPUT_DIRECTORY = True
 
